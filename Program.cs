a30d789735dca94155a560919cc2856b16350cec
Theodor Iseke <tiseke.tidlix@gmail.com>
2025-08-20 12:30:53 +0200

    Improved Event System - Added event types - Added OnFollowerReceived event

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using TwitchSharp;
+using TwitchSharp.Events;
 using TwitchSharp.Items;
 
 public class Program
@@ -7,10 +8,10 @@ public class Program
     {
         ClientConfig Config = new()
         {
-            ClientID = "",
-            ClientSecret = "",
+            ClientID = Variables.ClientID,
+            ClientSecret = Variables.ClientSecret,
             Redirect_uri = "https://localhost:3000",
-            Username = "",
+            Username = Variables.Username, // Username = Username of the acting user (bot)
             Scopes = [
                 "user:bot",
                 "user:read:chat",
@@ -21,11 +22,11 @@ public class Program
             ]
         };
         TwitchClient Client = new(Config);
-        TwitchUser channel = new TwitchUser(Client, "");
+        TwitchUser channel = new TwitchUser(Client, Variables.ChannelName); // ChannelName = username of twitch channel, where the bot is supposed to join
 
 
         var msg = new TwitchMessageBuilder(Client);
-        msg.WithContent("Joined Channel as Test bot");
+        msg.WithContent("Test");
         await msg.SendAsync(channel);
 
         Client.EventEngine.OnMessageReceived += async (s, e) =>
@@ -33,7 +34,15 @@ public class Program
             Console.WriteLine($"Received message > {e.Message.Channel} - {e.Message.Author}: {e.Message.Content} ({e.Message.ID})");
             if (e.Message.Content == "Ping") await e.Message.RespondAsync("Pong!");
         };
-        await Client.EventEngine.StartListeningForMessagesAsync(channel);
+        await Client.EventEngine.SubscribeToEventAsnyc(channel, new(EventType.MessageReceived));
+
+        Client.EventEngine.OnFollowReceived += async (s, e) =>
+        {
+            Console.Write("Follow Received: ");
+            Console.WriteLine($"{e.Follower.DisplayName}");
+            await e.Broadcaster.SendMessageAsync($"{e.Follower.DisplayName} just followed!");
+        };
+        await Client.EventEngine.SubscribeToEventAsnyc(channel, new(EventType.FollowReceived));
 
         while (true) ;
     }
